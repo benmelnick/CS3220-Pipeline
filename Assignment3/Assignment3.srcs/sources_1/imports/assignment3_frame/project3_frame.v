3f6db2c79b16dafaeba290ddeb23bf13641c3ff9
benmelnick <bmelnick3@gatech.edu>
2021-03-04 20:59:54 -0500

    Connect stages to stall units and remove unnecessary wires

--- Assignment3/Assignment3.srcs/sources_1/imports/assignment3_frame/project3_frame.v
+++ Assignment3/Assignment3.srcs/sources_1/imports/assignment3_frame/project3_frame.v
@@ -28,70 +28,77 @@ clk_wiz_0 my_clock
   
   /* wires to connect between pipeline stages */ 
   
+  /* Outputs of each stage */
   wire [`FE_latch_WIDTH-1:0] FE_latch_out; 
   wire [`DE_latch_WIDTH-1:0] DE_latch_out; 
   wire [`AGEX_latch_WIDTH-1:0] AGEX_latch_out; 
   wire [`MEM_latch_WIDTH-1:0] MEM_latch_out;
 
-  wire [`from_DE_to_FE_WIDTH-1:0] from_DE_to_FE;
+  /* Wires to pass data between stages within the same clock cycle */
+  //wire [`from_DE_to_FE_WIDTH-1:0] from_DE_to_FE;
   wire [`from_AGEX_to_FE_WIDTH-1:0] from_AGEX_to_FE;
-  wire [`from_MEM_to_FE_WIDTH-1:0] from_MEM_to_FE;
-  wire [`from_WB_to_FE_WIDTH-1:0] from_WB_to_FE;
+  //wire [`from_MEM_to_FE_WIDTH-1:0] from_MEM_to_FE;
+  //wire [`from_WB_to_FE_WIDTH-1:0] from_WB_to_FE;
 
-  wire [`from_AGEX_to_DE_WIDTH-1:0] from_AGEX_to_DE;
-  wire [`from_MEM_to_DE_WIDTH-1:0] from_MEM_to_DE;
+  //wire [`from_AGEX_to_DE_WIDTH-1:0] from_AGEX_to_DE;
+  //wire [`from_MEM_to_DE_WIDTH-1:0] from_MEM_to_DE;
   wire [`from_WB_to_DE_WIDTH-1:0] from_WB_to_DE;
 
-  wire [`from_MEM_to_AGEX_WIDTH-1:0] from_MEM_to_AGEX;
-  wire [`from_WB_to_AGEX_WIDTH-1:0] from_WB_to_AGEX;
+  //wire [`from_MEM_to_AGEX_WIDTH-1:0] from_MEM_to_AGEX;
+  //wire [`from_WB_to_AGEX_WIDTH-1:0] from_WB_to_AGEX;
 
-  wire [`from_WB_to_MEM_WIDTH-1:0] from_WB_to_MEM;
+  //wire [`from_WB_to_MEM_WIDTH-1:0] from_WB_to_MEM;
+
+  /* Wires for stall unit inputs and outputs */
+  wire [`from_DE_to_stall_WIDTH-1:0] from_DE_to_stall;
+  wire [`from_AGEX_to_stall_WIDTH-1:0] from_AGEX_to_stall;
+  wire [`from_MEM_to_stall_WIDTH-1:0] from_MEM_to_stall;
+  wire data_hazard;
+  wire control_hazard;
 
 
-
+STALL_UNIT my_stall_unit(
+  .from_DE_to_stall(from_DE_to_stall),
+  .from_AGEX_to_stall(from_AGEX_to_stall),
+  .from_MEM_to_stall(from_MEM_to_stall),
+  .data_hazard(data_hazard),
+  .control_hazard(control_hazard));
 
 
 FE_STAGE my_FE_stage(
     .clk(clk), 
     .reset(reset), 
-    .from_DE_to_FE(from_DE_to_FE),
     .from_AGEX_to_FE(from_AGEX_to_FE),
-    .from_MEM_to_FE(from_MEM_to_FE),
-    .from_WB_to_FE(from_WB_to_FE),
+    .data_hazard(data_hazard),
+    .control_hazard(control_hazard),
     .FE_latch_out(FE_latch_out)); 
                      
 DE_STAGE my_DE_stage(
   .clk(clk),
   .reset(reset),
   .from_FE_latch(FE_latch_out),
-  .from_AGEX_to_DE(from_AGEX_to_DE),  
-  .from_MEM_to_DE(from_MEM_to_DE),     
   .from_WB_to_DE(from_WB_to_DE), 
-  .from_DE_to_FE(from_DE_to_FE),   
+  .data_hazard(data_hazard),
+  .from_DE_to_stall(from_DE_to_stall),
   .DE_latch_out(DE_latch_out)
 );
 
 AGEX_STAGE my_AGEX_stage(
   .clk(clk),
   .reset(reset),
-  .from_MEM_to_AGEX(from_MEM_to_AGEX),    
-  .from_WB_to_AGEX(from_WB_to_AGEX),   
   .from_DE_latch(DE_latch_out),
   .AGEX_latch_out(AGEX_latch_out),
   .from_AGEX_to_FE(from_AGEX_to_FE),
-  .from_AGEX_to_DE(from_AGEX_to_DE)
+  .from_AGEX_to_stall(from_AGEX_to_stall)
 );
 
 MEM_STAGE my_MEM_stage(
   .clk(clk),
   .reset(reset),
   .KEY(KEY),
-  .from_WB_to_MEM(from_WB_to_MEM),  
   .from_AGEX_latch(AGEX_latch_out), 
   .MEM_latch_out(MEM_latch_out),
-  .from_MEM_to_FE(from_MEM_to_FE),
-  .from_MEM_to_DE(from_MEM_to_DE),
-  .from_MEM_to_AGEX(from_MEM_to_AGEX)
+  .from_MEM_to_stall(from_MEM_to_stall)
 );     
 
 
@@ -100,10 +107,7 @@ WB_STAGE my_WB_stage(
   .clk(clk),
   .reset(reset),  
   .from_MEM_latch(MEM_latch_out),
-  .from_WB_to_FE(from_WB_to_FE),
   .from_WB_to_DE(from_WB_to_DE),  
-  .from_WB_to_AGEX(from_WB_to_AGEX),
-  .from_WB_to_MEM(from_WB_to_MEM),
   .HEX0(HEX0),
   .HEX1(HEX1), 
   .LEDR(LEDR) 
