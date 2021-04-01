29f1fb4353f142257c39b76f6a018a918839cb61
benmelnick <bmelnick3@gatech.edu>
2021-04-01 14:11:05 -0400

    Delete some comments

--- bubblesort.asm
+++ bubblesort.asm
@@ -19,6 +19,7 @@ BadPC:
 	br	BadPC
 
 ; section of memory containing the array
+; load the array starting at memory address 0x040
 .ORG 0x040  
 .WORD 0x0010 ; initial array contents to sort
 .WORD 0x0050
@@ -32,17 +33,6 @@ BadPC:
 
 .ORG 0x100  ; code starts executing here - CPU has start PC of 0x100
 
-; step 1: load the array starting at memory address 0x040
-; mem[0x040] = 0x0010 
-; mem[0x044] = 0x0050 
-; mem[0x048] = 0x0012 
-; mem[0x04c] = 0x0003 
-; mem[0x050] = 0x0002 
-; mem[0x054] = 0x0001 
-; mem[0x058] = 0x0030 
-; mem[0x05c] = 0x0050 
-; mem[0x060] = 0x0085
+; step 1: perform bubblesort algorithm
 
-; step 2: perform bubblesort algorithm
-
-; step 3: iterate through sorted array and print values to HEX
+; step 2: iterate through sorted array and print values to HEX
