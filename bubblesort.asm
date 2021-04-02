00bd202b7298f2364f161a20d0de0d635b271e87
benmelnick <bmelnick3@gatech.edu>
2021-04-01 22:12:59 -0400

    Bug fixes for looping and array indices

--- bubblesort.asm
+++ bubblesort.asm
@@ -35,8 +35,8 @@ BadPC:
 
 ; step 1: perform bubblesort algorithm
 ; N = number of items in memory to sort
-; for j = 1 to N-1:
-;   for i = 1 to N-j-1:
+; for j = 0 to N-1:
+;   for i = 0 to N-j-1:
 ;     memA = mem[i]
 ;	    memB = mem[i+1]
 ;	    if memA > memB:
@@ -45,32 +45,38 @@ BadPC:
 ;		    mem[i+1] = memA
 
 BUBBLESORT:
-	addi zero, t0, SIZE		; t0 = SIZE
-	add  t1, zero, zero 	; t1 = 0, counter for outer loop
-	add  s0, zero, zero 	; s0 = 0, counter for inner loop
+  addi zero, s2, 0x2    ; s2 = 2
+	add  t0, zero, zero 	; t0 = 0, counter for outer loop (j)
+  addi zero, s0, SIZE		; s0 = SIZE - 1 (terminating condition for outer loop)
+  addi s0, s0, -0x1
 
 INNERLOOP:
-	lw a0, ARRAY(s0)		; a0 = array[s0], first word in array
-	lw a1, ARRAY(s0 + 0x4)	; a1 = array[s0 + 4], next word in array
-	blt a0, a1, INCREMENT   ; check if a1 < a0, if it is then swap, if not break
-	lw a2, ARRAY(s0 + 0x4)	; load a1 into temp
-	sw a0, ARRAY(s0 + 0x4)	; store a0 into a1
-	sw a2, ARRAY(s0)		; store temp into a0
+  add  t1, zero, zero 	 ; t1 = 0, counter for inner loop (i)
+  sub  s1, s0, t0        ; s1 = SIZE - j - 1 (need to compute terminating condition for inner loop)
+COMPARE:    
+  lshf a2, t1, s2        ; a2 = i * 4 (word-aligned offset from start of array)
+  lw   a0, ARRAY(a2)     ; a0 = array[i], first word in array
+  addi a2, a2, 0x4       ; compute offset for next word in array
+  lw   a1, ARRAY(a2)     ; a1 = array[i + 1]
+  blt  a0, a1, INCREMENT ; if a0 < a1, already sorted, move onto next iteration - else, swap
+  sw   a0, ARRAY(a2)     ; array[i + 1] = a0 
+  addi a2, a2, -0x4      ; get array offset for index i
+  sw   a1, ARRAY(a2)     ; array[i] = a1
 INCREMENT:
-	add	s0, s0, 0x4			; swap is done so increment inner counter 
-	blt s0, t0, INNERLOOP	; loop is s0 < SIZE
+  addi t1, t1, 0x1       ; increment inner loop counter
+  blt  t1, s1, COMPARE   ; if i < SIZE - j - 1, move onto next iteration of the loop if not done
 
 ;OUTERLOOP - not really needed as a label
-	add t1, t1, 0x4			; once inner loop counter is done increment outer loop counter
-	blt t1, t0, INNERLOOP	; go on to next iteration of innerloop
+	addi t0, t0, 0x1			  ; once inner loop counter is done increment outer loop counter
+	blt  t0, s0, INNERLOOP	; if j < SIZE - 1, go on to next iteration of innerloop
 
 ; step 2: iterate through sorted array and print values to HEX
   addi zero, a0, 0x2   ; a0 = 2 - used to calculate word-aligned offset later
   addi zero, t0, SIZE  ; t0 = SIZE
-  add  t1, zero, zero  ; t1 = 0
+  add  t1, zero, zero  ; t1 = 0 - counter for array index
 Print:
   lshf s0, t1, a0      ; s0 = t1 * 4 
-  lw   s1, ARRAY(t1)   ; s1 = array[s0]
+  lw   s1, ARRAY(s0)   ; s1 = array[s0]
   sw   s1, HEX(zero)   ; print value to HEX
   sw   t1, LEDR(zero)  ; print the index of the array to LEDR
   addi t1, t1, 0x1     ; increment index
@@ -80,4 +86,4 @@ Print:
   
 ; loop forever once the algorithm is complete  
 Done:
-  br   DONE  
+  br   Done  
